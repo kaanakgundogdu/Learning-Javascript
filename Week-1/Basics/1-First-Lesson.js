c58daa865b16537ac31dbe5169d972fae8942dfc
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-04-12 16:47:16 +0300

    First lesson.js changed.

--- Week-1/Basics/1-First-Lesson.js
+++ Week-1/Basics/1-First-Lesson.js
@@ -1,3 +1,27 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN');
-console.log(40 - 70);
+
+
+
+// 1. Number: floating point numbers let childreCountn=10;
+//2. String: Sequence for chaacters let children="velet";
+//3.Boolean: Logical type let isChildren=true;
+//4. Undefined: empty value.  let children;
+//5. Null: Empty value
+//6. Symbol: Value unique and cannot be changed
+//7. BigInt: Larger integers
+
+alert("I'm learning JavaScript");
+
+console.log(typeof true);
+console.log(typeof "Kaan");
+console.log(typeof 22);
+
+let age = 22;
+console.log(typeof age);
+age = 23;
+console.log(typeof age);
+age = 'kaan'
+console.log(typeof age);
+
+const birthYear = 1995;
+//birthYear=2; gives you an error
+//const job; give error too
