247a86f953bc6e737b7a862f7c453895fd994b3d
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-04-28 22:47:08 +0300

    Lesson 4 added.

---
+++ Week-1/Basics/4.Learning-strings.js
@@ -0,0 +1,13 @@
+const name = 'Kaan';
+const job = 'Learner';
+const birthYear = 1995;
+const currentYear = 2022;
+
+const presentation = " I'm  " + name + ', a ' + (currentYear - birthYear) + ' years old ' + job + '!';
+
+console.log(presentation);
+
+const presentation_New = `I'm ${name}, a ${currentYear - birthYear}, years old ${job}! `;
+
+console.log(presentation_New);
+
