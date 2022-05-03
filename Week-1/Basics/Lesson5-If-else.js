611ccdb83690202922d4307caf0bd52b8b042ad1
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-03 16:18:04 +0300

    Lesson 5 added.

---
+++ Week-1/Basics/Lesson5-If-else.js
@@ -0,0 +1,22 @@
+
+const age = 15;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+    console.log("You can start driving license 🚗🚚🛴🚲🛵")
+}
+else {
+    const yearsLeft = 18 - age;
+    console.log(`You are too young wait for ${yearsLeft} years 💕💖🤸‍♂️`)
+}
+
+const birthYear = 1995;
+let century;
+
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
