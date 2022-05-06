5b7b267c0e0cd1a346d57ddc315eab53b2a215b3
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-06 16:30:39 +0300

    The Conditional (Ternary) Operator lesson added.

---
+++ Week-1/Basics/Lesson11-ConditionalOperator.js
@@ -0,0 +1,16 @@
+
+
+const age = 19;
+
+age >= 18 ? console.log("You can drive!") :
+    console.log("You can't drive!!!!");
+
+
+const typeOfSchool = age >= 18 ? 'University😢' : 'High school😁';
+
+console.log(`You can go to ${typeOfSchool}!!!!`);
+
+
+//You can use expression on template literal
+
+console.log(`I'm an ${age >= 18 ? 'University😢' : 'High school😁'} student.`);
