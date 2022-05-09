18ef583fd076b6292a24d85ab1c5f8702f6e93b3
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-09 15:58:36 +0300

    Added Developer Skills and tools folder.

---
+++ 2-DeveloperSkillsandTools/Lesson1-VsCodeSetup.js
@@ -0,0 +1,16 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+//add Prettier extension on vscode
+//Go settings and search for default formatter than select prettier
+//If you didnt click format on save than search in setting and enable format on save
+
+const newValue = 2;
+//if you declare a ariable and save on not pretty position it automaticly change line on save
+const calcAge = (birtYear) => 2022 - birtYear;
+//Add code snippet for console.log() i made a cl prefix
+console.log(newValue);
+//Than i added TODO Highlight extension its looking good
+//You can open setttings on json format and add highligt commands
+//https://github.com/wayou/vscode-todo-highlight/issues/93
+//FIXME BUG todo
