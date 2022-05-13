224f88a4b6b93651ed4b3e6a0b732c91bb70de5b
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-13 15:34:10 +0300

    Roll dice mech. added.

--- 4-DOMandEventsFundamental/3-Pig-Game/script.js
+++ 4-DOMandEventsFundamental/3-Pig-Game/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+//Selecting elements
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1'); //only works for id
+const diceElement = document.querySelector('.dice');
+const btnNewGame = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const player0CurrentElement = document.querySelector('#current--0');
+const player1CurrentElement = document.querySelector('#current--1');
+let currentScore = 0;
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
+
+btnRoll.addEventListener('click', function () {
+  const randomDiceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  diceElement.classList.remove('hidden');
+  diceElement.src = `images/dice-${randomDiceNumber}.png`;
+
+  if (randomDiceNumber !== 1) {
+    currentScore += randomDiceNumber;
+    player0CurrentElement.textContent = currentScore;
+  } else {
+  }
+});
