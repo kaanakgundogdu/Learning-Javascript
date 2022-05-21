1e62888f81174aabbcf43ad8010824d3b3e37ba3
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-05-21 21:01:15 +0300

    Data-Structures-Operators added.

---
+++ 6-Data-Structures-Operators/1-DestructuringArrays.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.categories[mainIndex]];
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//Destructring assignemn
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+//alternative
+const [one, , three] = restaurant.categories;
+console.log(first, second);
+console.log(one, three);
+
+let [main, secondary] = restaurant.categories;
+
+const temp = main; //same but long
+main = secondary;
+secondary = temp;
+
+console.log(main, secondary);
+
+[secondary, main] = restaurant.categories; //same  and short
+console.log(main, secondary);
+
+//recive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//Nested destructuring
+const nested = [2, 4, [5, 6]];
+
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+//if you dont sure about array size
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
